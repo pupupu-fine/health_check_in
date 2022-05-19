9c5dbf681e0f081ff50e2485098ff625bfec15ef
szr <zirui990712@163.com>
2022-05-19 10:36:15 +0800

    修改2

--- hitcarder.py
+++ hitcarder.py
@@ -147,7 +147,7 @@ class HitCarder(object):
         new_info['sfzx'] = old_info['sfzx'] # 在校
         new_info['sfymqjczrj'] = old_info['sfymqjczrj'] # 入境
         new_info['sfqrxxss'] = 1 # 属实
-        new_info['verifyCode'] = self.get_captcha()
+        # new_info['verifyCode'] = self.get_captcha()
 
         self.info = new_info
         # print(json.dumps(self.info))
